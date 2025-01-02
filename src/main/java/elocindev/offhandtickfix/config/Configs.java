3ccfbb6b9fa7b55bc036150c446959d089ebe3c8
nvb-uy <elocindev@lunapixel.gg>
2025-01-01 22:21:55 -0300

    fix(path and config load)

--- src/main/java/elocindev/offhandtickfix/config/Configs.java
+++ src/main/java/elocindev/offhandtickfix/config/Configs.java
@@ -4,10 +4,11 @@ import elocindev.offhandtickfix.config.entries.OffHandTickFixConfig;
 import elocindev.necronomicon.api.config.v1.NecConfigAPI;
 
 public class Configs {
-    public static OffHandTickFixConfig MAIN = OffHandTickFixConfig.INSTANCE;
+    public static OffHandTickFixConfig MAIN;
 
     public static void loadCommonConfigs() {
         NecConfigAPI.registerConfig(OffHandTickFixConfig.class);
+        MAIN = OffHandTickFixConfig.INSTANCE;
     }
 
     public static void loadClientConfigs() {}
